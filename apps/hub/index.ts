19d5c3c0ca8fdb68f47a9807efe21eb9f561bb47
Nihallllll <nihal24rajak@gmail.com>
2025-08-07 22:42:54 +0530

    added functions

--- apps/hub/index.ts
+++ apps/hub/index.ts
@@ -10,7 +10,7 @@ const availableValidators: { validatorId: string, socket: ServerWebSocket<unknow
 const CALLBACKS : { [callbackId: string]: (data: IncomingMessage) => void } = {}
 const COST_PER_VALIDATION = 100; // in lamports
 
-Bun.serve({
+ Bun.serve({
     fetch(req, server) {
       if (server.upgrade(req)) {
         return;
@@ -31,9 +31,122 @@ Bun.serve({
             return;
           }
         } else if(data.type === 'validate') {
-           CALLBACKS[data.data.callbackId](data);
+           CALLBACKS[data.data.callbackId]?(data.data);
            delete CALLBACKS[data.data.callbackId];
         }
       }
     }
   });
+
+
+  export async function verifyMessage(message: string, publicKey: string, signedMessage: string) {
+    const messageBytes = nacl_util.decodeUTF8(message);
+    const result = nacl.sign.detached.verify(
+        messageBytes,
+        new Uint8Array(JSON.parse(signedMessage)),
+        new PublicKey(publicKey).toBytes(),
+    );
+    return result;
+  }
+    
+
+  async function signupHandler(ws: ServerWebSocket<unknown>, { ip, publicKey, signedMessage, callbackId }: SignupIncomingMessage) {
+    const validatorDb = await prismaClient.validator.findFirst({
+        where: {
+            publicKey,
+        },
+    });
+    
+
+    if(validatorDb){
+        ws.send(JSON.stringify({
+            type: 'signup',
+            data: {
+                validatorId: validatorDb.id,
+                callbackId,
+            }
+        }));
+
+        availableValidators.push({
+            validatorId: validatorDb.id,
+            socket: ws,
+            publicKey: validatorDb.publicKey,
+        });
+        
+    }
+
+
+
+    const validator = await prismaClient.validator.create({
+        data: {
+            publicKey,
+            ip,
+            location : "unknown",
+        },
+    });
+    ws.send(JSON.stringify({
+        type: 'signup',
+        data: {
+            validatorId: validator.id,
+            callbackId,
+        }
+    }));
+
+    availableValidators.push({
+        validatorId: validator.id,
+        socket: ws,
+        publicKey: validator.publicKey,
+    });
+
+}
+
+setInterval(async () => {
+    const websitesToMonitor = await prismaClient.website.findMany({
+        where: {
+            disabled: false,
+        }
+    });
+
+    for(const website of websitesToMonitor) {
+        availableValidators.forEach(async (validator) => {
+            const callbackId = randomUUIDv7();
+            validator.socket.send(JSON.stringify({
+                type: 'validate',
+                data: {
+                    url: website.url,
+                    callbackId,
+                    websiteId: website.id,
+                }
+            }));
+            CALLBACKS[callbackId] = (data : IncomingMessage) => {
+                if(data.type === 'validate') {
+                    const { status, latency, websiteId, validatorId } = data.data;
+                    const verfied = await verifyMessage(
+                        `Validate message from ${validatorId}`,
+                        validator.publicKey,
+                        data.data.signedMessage
+                    );
+                    if(!verfied) {
+                        return;
+                    }
+
+                    await prismaClient.$transaction(async (tx:any) => {
+                        await tx.websiteTick.create({
+                            data: {
+                                websiteId: website.id,
+                                validatorId,
+                                status,
+                                latency,
+                                createdAt: new Date(),
+                            },
+                        });
+
+                        await tx.validator.update({
+                            where: { id: validatorId },
+                            data: {
+                                pendingPayouts: { increment: COST_PER_VALIDATION },
+                            },
+                        });
+                    });
+                }
+} , 60*1000);
